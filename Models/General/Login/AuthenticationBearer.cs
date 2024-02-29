611001f95cc049b6cfaefbf2640aed6f9c0f4c42
Mohammad Salman <mssallu546789@gmail.com>
2024-02-29 14:00:47 +0500

    Added User Management Functional

---
+++ Models/General/Login/AuthenticationBearer.cs
@@ -0,0 +1,9 @@
+﻿namespace VehicleManagement.Models.General.Login
+{
+    public class AuthenticationBearer
+    {
+        public string bearer { get; set; }
+        public string role { get; set; }
+        public string remarks { get; set; }
+    }
+}
