f6aabb5ff55cdcf97ae4ba26570a1acc5c140c65
Mohammad Salman <mssallu546789@gmail.com>
2024-03-07 12:25:23 +0500

    Added Driver Functionality

---
+++ Models/General/Driver/AddDriver.cs
@@ -0,0 +1,11 @@
+﻿namespace VehicleManagement.Models.General.Driver
+{
+    public class AddDriver
+    {
+        public string DriverID { get; set; }
+        public string DriverName { get; set; }
+        public string Contact { get; set; }
+        public string Designation { get; set; }
+        public bool isActive { get; set; }
+    }
+}
---
+++ Models/General/Driver/AllDriver.cs
@@ -0,0 +1,12 @@
+﻿namespace VehicleManagement.Models.General.Driver
+{
+    public class AllDriver
+    {
+        public int driverdbID { get; set; }
+        public string driverID { get; set; }
+        public string driverName { get; set; }
+        public string contact { get; set; }
+        public string designation { get; set; }
+        public bool isActive { get; set; }
+    }
+}
