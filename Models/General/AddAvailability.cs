3633c509b2f3170dbe1e2c4310a694767a184310
Mohammad Salman <mssallu546789@gmail.com>
2024-03-13 14:15:23 +0500

    Added Availability Changes

---
+++ Models/General/AddAvailability.cs
@@ -0,0 +1,13 @@
+﻿namespace VehicleManagement.Models.General
+{
+    public class AddAvailability
+    {
+        public int Driver1ID { get; set; }
+        public int Driver2ID { get; set; }
+        public string? VehicleID { get; set; }
+        public string? VehicleLocation { get; set; }
+        public string? Business { get; set; }
+        public DateTime AvailDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
