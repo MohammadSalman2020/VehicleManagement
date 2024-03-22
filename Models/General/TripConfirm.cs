d4f53d5db413b4a9cfa2a8749da14eb63b17e986
Mohammad Salman <mssallu546789@gmail.com>
2024-03-22 14:38:57 +0500

    Added Trip Changes

---
+++ Models/General/TripConfirm.cs
@@ -0,0 +1,16 @@
+﻿namespace VehicleManagement.Models.General
+{
+    public class TripConfirm
+    {
+        public int loadID { get; set; }
+        public string vehicleID { get; set; }
+        public string business { get; set; }
+        public int businessID { get; set; }
+
+        public string loadingPoint { get; set; }
+        public string decentingPoint { get; set; }
+        public DateTime loadAssignDate { get; set; }
+        public string driver1 { get; set; }
+        public string driver2 { get; set; }
+    }
+}
