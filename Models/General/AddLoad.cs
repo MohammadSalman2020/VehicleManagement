e390aa28e00bfaacbf23ef22d51df7a26874a96a
Mohammad Salman <mssallu546789@gmail.com>
2024-03-28 22:30:25 +0500

    Added Modified Changes

--- Models/General/AddLoad.cs
+++ Models/General/AddLoad.cs
@@ -5,6 +5,7 @@
         public int availID { get; set; }
         public string loading { get; set; }
         public string decenting { get; set; }
+        public string product { get; set; }
         public DateTime dated { get; set; }
         public int businessID { get; set; }
         public bool isActive { get; set; } = true;
