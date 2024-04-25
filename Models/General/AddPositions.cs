cc10723d754d148b2e932fb1c88c35b44364c717
Mohammad Salman <mssallu546789@gmail.com>
2024-04-25 11:49:44 +0500

    Added Code to Asign Vehicle Positions

---
+++ Models/General/AddPositions.cs
@@ -0,0 +1,9 @@
+﻿namespace VehicleManagement.Models.General
+{
+    public class AddPositions
+    {
+        public string VehicleID { get; set; }
+        public string Position { get; set; }
+        public string Remarks { get; set; }
+    }
+}
