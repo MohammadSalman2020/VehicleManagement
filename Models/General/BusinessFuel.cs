bea2e22d2248dba739ba19ea66e5cabcdbf37b5c
Mohammad Salman <mssallu546789@gmail.com>
2024-06-28 10:51:14 +0500

    Modified Code in Business Fuel

---
+++ Models/General/BusinessFuel.cs
@@ -0,0 +1,9 @@
+﻿namespace VehicleManagement.Models.General
+{
+    public class BusinessFuel
+    {
+        public int locationID { get; set; }
+        public string locationName { get; set; }
+        public int busID { get; set; }
+    }
+}
